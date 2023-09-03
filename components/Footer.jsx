75436d8c3930e673e9e49a236674a9b393b350b6
abdulhadi240 <ah912425@gmail.com>
2023-09-03 13:57:59 +0500

    changes

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -38,9 +38,7 @@ const Footer = () => (
           <h4 className="font-extrabold text-[24px] text-white">
             METAVERUS
           </h4>
-          <p className="font-normal text-[14px] text-white opacity-50">
-            Copyright © 2021 - 2022 Metaversus. All rights reserved.
-          </p>
+          
 
           <div className="flex gap-4">
             {socials.map((social) => (
